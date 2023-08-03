0a574be097e24b034b463f84f19142c6d9cbdffa
Roberto Medina P <robertomedinamv@gmail.com>
2023-08-02 23:20:59 -0400

    creando viewmodel

---
+++ app/src/main/java/com/example/marsrealstate/ViewModel/MarsViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.marsrealstate.ViewModel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class MarsViewModel(application: Application): AndroidViewModel(application) {
+
+    
+}
